40390d05856e1eaaa4df81980b0da37174345ae0
user <km_413@naver.com>
2021-05-04 18:34:45 +0900

    add view & function

--- app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,57 @@
 package com.example.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityAddHappyPlaceBinding
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            setSupportActionBar(toolbarAddPlace)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            toolbarAddPlace.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+            etDate.setOnClickListener(this@AddHappyPlaceActivity)
+        }
+
+
+    }
+
+    // onClick()를 각각 정해주지 않고 통합해서 사용 when id로 구분
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date->{
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(cal.time).toString())
     }
 }
--- app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            fabAddHappyPlace.setOnClickListener {
+                startActivity(Intent(this@MainActivity, AddHappyPlaceActivity::class.java))
+            }
+        }
     }
 }
